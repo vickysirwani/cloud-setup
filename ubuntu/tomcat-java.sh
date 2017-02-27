726c3150561c7dd7c4571f5aeba18201a6877d05
vickysirwani <vickysirwani@gmail.com>
2017-02-27 11:24:07 +0530

    Update tomcat-java.sh

--- ubuntu/tomcat-java.sh
+++ ubuntu/tomcat-java.sh
@@ -1,7 +1,7 @@
 export DEBIAN_FRONTEND=noninteractive
-sudo apt-get -y update
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
 sudo add-apt-repository ppa:webupd8team/java -y
+sudo apt-get -y update
 sudo apt-get -y install mysql-client
 sudo apt-get -y install git
 sudo apt-get -y install oracle-java8-installer
