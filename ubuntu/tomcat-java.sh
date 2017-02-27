e84c3e35edb54f96d8ad90b1667745978f89fc31
vickysirwani <vickysirwani@gmail.com>
2017-02-27 11:21:17 +0530

    Update tomcat-java.sh

--- ubuntu/tomcat-java.sh
+++ ubuntu/tomcat-java.sh
@@ -6,3 +6,5 @@ sudo apt-get -y install mysql-client
 sudo apt-get -y install git
 sudo apt-get -y install oracle-java8-installer
 sudo apt-get -y install tomcat8
+sudo apt-get -y install maven
+sudo apt-get -y install gradle
