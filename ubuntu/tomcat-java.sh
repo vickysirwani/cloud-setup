210a5f452cc44d563fb26c74f1b70fa87af66223
vickysirwani <vickysirwani@gmail.com>
2017-02-27 11:23:26 +0530

    Update tomcat-java.sh

--- ubuntu/tomcat-java.sh
+++ ubuntu/tomcat-java.sh
@@ -8,3 +8,4 @@ sudo apt-get -y install oracle-java8-installer
 sudo apt-get -y install tomcat8
 sudo apt-get -y install maven
 sudo apt-get -y install gradle
+sudo apt-get -y install tomcat8-docs tomcat8-admin tomcat8-examples
